a8fa71e858cef820f8c1c606f92f3ff92b48950e
redy77 <redy@yandex.ru>
2021-08-21 23:42:22 +0300

    jdbcConnection

--- Lesson25_Starter.SpringJDBC/src/main/java/ru/alishev/springcourse/dao/PersonDAO.java
+++ Lesson25_Starter.SpringJDBC/src/main/java/ru/alishev/springcourse/dao/PersonDAO.java
@@ -12,7 +12,7 @@ import java.util.List;
  */
 @Component
 public class PersonDAO {
-    private static int PEOPLE_COUNT;
+//    private static int PEOPLE_COUNT;
     private static final String URL = "jdbc:mysql://localhost:3306/taskjdbc";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "root-123";
@@ -56,15 +56,36 @@ public class PersonDAO {
     }
 
     public Person show(int id) {
+        Person person = null;
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement("select * from person where id=?");
+            preparedStatement.setInt(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            resultSet.next();
+            person = new Person();
+
+            person.setId(resultSet.getInt("id"));
+            person.setName(resultSet.getString("name"));
+            person.setAge(resultSet.getInt("age"));
+            person.setEmail(resultSet.getString("email"));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return person;
+
 //        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
-        return null;
+//        return null;
     }
 
     public void save(Person person) {
         try {
-            Statement statement = connection.createStatement();
-            String sql = "INSERT INTO PERSON VALUES(" + 1 + ",'" + person.getName() + "'," +  person.getAge() + ",'" + person.getEmail() + "')";
-            statement.executeUpdate(sql);
+            PreparedStatement statement = connection.prepareStatement("insert into person values(1, ?, ?, ?)");
+            statement.setString(1, person.getName());
+            statement.setInt(2, person.getAge());
+            statement.setString(3, person.getEmail());
+
+//            String sql = "INSERT INTO PERSON VALUES(" + 1 + ",'" + person.getName() + "'," +  person.getAge() + ",'" + person.getEmail() + "')";
+            statement.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -72,15 +93,33 @@ public class PersonDAO {
 //        people.add(person);
     }
 
-//    public void update(int id, Person updatedPerson) {
-//        Person personToBeUpdated = show(id);
-//
+    public void update(int id, Person updatedPerson) {
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement("update person set name=?, age=?, email=? where id=?");
+
+            preparedStatement.setString(1, updatedPerson.getName());
+            preparedStatement.setInt(2, updatedPerson.getAge());
+            preparedStatement.setString(3, updatedPerson.getEmail());
+            preparedStatement.setInt(4, id);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+//Person personToBeUpdated = show(id);
 //        personToBeUpdated.setName(updatedPerson.getName());
 //        personToBeUpdated.setAge(updatedPerson.getAge());
 //        personToBeUpdated.setEmail(updatedPerson.getEmail());
-//    }
+    }
 
     public void delete(int id) {
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement("delete from person where id=?");
+            preparedStatement.setInt(1, id);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
 //        people.removeIf(p -> p.getId() == id);
     }
 }
